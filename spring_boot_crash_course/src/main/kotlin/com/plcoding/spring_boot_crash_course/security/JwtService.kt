0e204efb81a1f3efd7f432f4d959101eae7b25d6
Maximiliano Vilariño <mvilarino@aldaba.es>
2026-04-24 15:20:22 +0200

    JWT configuration

---
+++ spring_boot_crash_course/src/main/kotlin/com/plcoding/spring_boot_crash_course/security/JwtService.kt
@@ -0,0 +1,36 @@
+package com.plcoding.spring_boot_crash_course.security
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import java.util.Base64
+import java.util.Date
+
+@Service
+class JwtService(
+    @Value("JWT_SECRET_BASE64") private val jwtSecret: String
+) {
+
+    private val secretKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(jwtSecret))
+    private val accessTokenValidityMs = 15L * 60L * 1000L
+    private val refreshTokenValidityMs = 30L * 24 * 60 * 60 * 1000L
+
+    private fun generateToken(
+        userId: String,
+        type: String,
+        expiry: Long
+    ): String {
+        val now = Date()
+        val expiryDate = Date(now.time + expiry)
+        return Jwts.builder()
+            .subject(userId)
+            .claim("type", type)
+            .issuedAt(now)
+            .expiration(expiryDate)
+            .signWith(secretKey, Jwts.SIG.HS256)
+            .compact()
+    }
+
+
+}
