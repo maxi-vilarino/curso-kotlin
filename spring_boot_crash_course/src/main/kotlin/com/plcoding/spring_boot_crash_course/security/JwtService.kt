2f6df0d995acf255d57bba627b45e09bc1fb33a6
Maximiliano Vilariño <mvilarino@aldaba.es>
2026-04-27 11:21:50 +0200

    JWT Service and Auth filter

--- spring_boot_crash_course/src/main/kotlin/com/plcoding/spring_boot_crash_course/security/JwtService.kt
+++ spring_boot_crash_course/src/main/kotlin/com/plcoding/spring_boot_crash_course/security/JwtService.kt
@@ -1,5 +1,6 @@
 package com.plcoding.spring_boot_crash_course.security
 
+import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
@@ -30,6 +31,46 @@ class JwtService(
             .expiration(expiryDate)
             .signWith(secretKey, Jwts.SIG.HS256)
             .compact()
+    }
+
+    fun generateAccessToken(userId: String): String {
+        return generateToken(userId, "access", accessTokenValidityMs)
+    }
+
+    fun generateRefreshToken(userId: String): String {
+        return generateToken(userId, "refresh", refreshTokenValidityMs)
+    }
+
+    fun validateAccessToken(token: String): Boolean {
+        val claims = parseAllClaims(token) ?: return false
+        val tokenType = claims["type"] as? String ?: return false
+        return tokenType == "access"
+    }
+
+    fun validateRefreshToken(token: String): Boolean {
+        val claims = parseAllClaims(token) ?: return false
+        val tokenType = claims["type"] as? String ?: return false
+        return tokenType == "refresh"
+    }
+
+    fun getUserIdFromToken(token: String): String {
+        val claims = parseAllClaims(token) ?: throw IllegalArgumentException("invalid token")
+        return claims.subject
+    }
+
+    private fun parseAllClaims(token: String): Claims? {
+        val rawToken = if(token.startsWith("Bearer")) {
+            token.removePrefix("Bearer")
+        } else token
+        return try {
+            Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .payload
+        } catch(e: Exception) {
+            null
+        }
     }
 
 
